72f4058422dd79412e54517acbe990831a9fef31
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-02-10 18:07:52 +0300

    Update main.c

--- Practice2.0/main.c
+++ Practice2.0/main.c
@@ -2,33 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "matrix.h"
-#define N 4 
+#define N 3 
+#define number 123
 
 int main() {
 	int i = 0;
 	int j = 0;
 	Tmatrix* m1, * m2, *res;
-	alloc_matrix(&m1, N,N);
-	alloc_matrix(&m2, N, n);
+	alloc_matrix(&m1, N);
+	alloc_matrix(&m2, N);
 	fill_matrix(m1);
 	fill_matrix(m2);
-	printf("Result of sum of matrices with each other");
+	printf("Introduced matrices:\n");
+	print_matrix(m1);
+	print_matrix(m2);
+	printf("Result of sum of matrices with each other\n");
 	res = add_matrix(m1, m2);
 	print_matrix(res);
 	free_matrix(&res);
-	printf("Result of the sum of the first matrix each with a constant");
-	res = add_number(m1, 2.5);
+	printf("Result of the sum of the first matrix each with a constant\n");
+	res = add_number(m1, number);
 	print_matrix(res);
 	free_matrix(&res);
-	printf("Result of matrix multiplication with each other");
+	printf("Result of matrix multiplication with each other\n");
 	res =multi_matrix(m1, m2);
 	print_matrix(res);
 	free_matrix(&res);
-	printf("Result of multiplying the first matrix by a constant");
-	res = multi_number(m1, 2.0);
+	printf("Result of multiplying the first matrix by a constant\n");
+	res = multi_number(m1, number);
 	print_matrix(res);
 	free_matrix(&res);
-	print_matrix(m1);
-	print_matrix(m2);
 	return 0;
 }
