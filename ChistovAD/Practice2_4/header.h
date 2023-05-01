aa6529e045f74593413f98c50e2b66420822b784
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-05-01 14:28:18 +0300

    Update header.h

--- ChistovAD/Practice2_4/header.h
+++ ChistovAD/Practice2_4/header.h
@@ -1,13 +1,22 @@
+#ifndef  HEADER_H
+#define _HEADER_H
+#include <string>
+#include <set>
+#include <vector>
+using namespace std;
+
 template <typename T>
 class Tcontainer {
 private:
-	T* elements;
+	vector T* elements;
 	int max_size;
 	int size:
 	int step;
 	int pas;
 	/*realloc*/
 public:
-
+	void set(size_t step, T item);
+	T get(size_t step) const;
 
 };
+#endif 
