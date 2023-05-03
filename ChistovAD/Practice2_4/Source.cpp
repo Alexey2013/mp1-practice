a2bae6a6d8a9890a0ff0b7813e9977291deca813
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-05-03 21:10:36 +0300

    Update Source.cpp

--- ChistovAD/Practice2_4/Source.cpp
+++ ChistovAD/Practice2_4/Source.cpp
@@ -18,3 +18,51 @@ string get_path() {
 		}
 	} while (true);
 }
+
+int Container::amount(const string& path)
+{
+	fstream file;
+	file.open(path);
+	string line;
+	int count{ 0 };
+	ifstream in(path);
+	while (getline(in, line))
+	{
+		if (line != "\0") {
+			count++;
+		}
+	}
+	in.close();
+	return count;
+}
+
+void menu() {
+	cout << "MENU:" << endl;
+	cout << "1.Scan a product" << endl;
+	cout << "2.Delete a product" << endl;
+	cout << "3.Add next product" << endl;
+	cout << "4.Calculate the amount" << endl;
+	cout << "5.Finish shopping" << endl;
+	cout << "6.New check" << endl;
+	cout << "Your choice:";
+	int answer1;
+	cin << answer1;
+	switch (answer1) {
+	case 1: 
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	
+	
+	}
+}
+
+TContainer::TContainer(int max_size, int step,const string& path) {
+	this->max_size = amount(path);
+	this->step = step;
+	element = new elements[this->max_size];
+
+
+}
