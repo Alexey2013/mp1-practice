9ff03fafb43ad8295820be6853b02ba6c3829d52
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-05-03 18:19:28 +0300

    Update header.h

--- ChistovAD/Practice2_4/header.h
+++ ChistovAD/Practice2_4/header.h
@@ -1,22 +1,47 @@
 #ifndef  HEADER_H
 #define _HEADER_H
 #include <string>
-#include <set>
-#include <vector>
 using namespace std;
 
 template <typename T>
-class Tcontainer {
+class Container {
 private:
-	vector T* elements;
+	elements *elements;
 	int max_size;
 	int size:
 	int step;
 	int pas;
-	/*realloc*/
 public:
-	void set(size_t step, T item);
-	T get(size_t step) const;
+	Container(int max_size, int step);
+};
 
+class Receipt {
+private:
+	long index;
+	TDate* date;
+	TTime* time;
+	TContainer* products;
+public:
+};
+
+class Recipline {
+public:
+	int num;
+	double sum;
+	TProduct* product;
+};
+
+class Date {
+public:
+	int year;
+	int month;
+	int day;
+};
+
+class Time {
+public:
+	int hour;
+	int minute;
+	int second;
 };
 #endif 
